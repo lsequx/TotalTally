1721c3e46f934422f1aa68c3cca40b1052f9e360
lsequx <lokeshsequeira@gmail.com>
2025-03-26 15:34:35 -0400

    updated files and folders

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <div className='container'>
+            <img src='../assets/logo.png' />
+        </div>
+    );
+}
+
+export default Header;
